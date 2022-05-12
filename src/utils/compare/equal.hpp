fd4df45cfefea421f1f7f47ddab2681b5119e4a4
scarf <greenscarf005@gmail.com>
2022-05-12 16:16:17 +0900

    feat: :sparkles: add iterator class

---
+++ src/utils/compare/equal.hpp
@@ -0,0 +1,8 @@
+#ifndef COMPARE_EQUAL_HPP
+#define COMPARE_EQUAL_HPP
+
+namespace ft {
+
+}  // namespace ft
+
+#endif // COMPARE_EQUAL_HPP
---
+++ src/utils/iterator/tag.hpp
@@ -0,0 +1,17 @@
+#ifndef ITERATOR_TAG_HPP
+#define ITERATOR_TAG_HPP
+
+namespace ft {
+
+/**
+ * @brief empty tag to identify categories of iterators.
+ */
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag {};
+struct bidirectional_iterator_tag {};
+struct random_access_iterator_tag {};
+
+}  // namespace ft
+
+#endif // ITERATOR_TAG_HPP
