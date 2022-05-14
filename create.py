a4b4446b9b1a56ebd5be799025b71cb26708e5d6
scarf <greenscarf005@gmail.com>
2022-05-14 12:55:02 +0900

    feat: :sparkles: better argument parsing using docopt

--- create.py
+++ create.py
@@ -1,33 +1,50 @@
 #!/bin/env python3
+"""create boilerplate for your c++ project
+
+Usage:
+  ./create.py [-h] [--parent=<parent>] [--namespace=<ns>] <path>
+  ./create.py (-h | --help)
+
+Options:
+  -n , --namespace <ns>     namespace to use [default: ft]
+  -p , --parent <parent>    root of directory path [default: src]
+
+"""
 
 from pathlib import Path
 from sys import argv
+
+from docopt import docopt
 
 template = """\
 #ifndef {name}{ext}
 #define {name}{ext}
 
-namespace ft {{
+namespace {namespace} {{
 
-}}  // namespace ft
+}}  // namespace {namespace}
 
 #endif // {name}{ext}
 """
 
 
 def main():
-    if len(argv) != 2:
-        print("Usage: create.py <path>")
-        return
+    assert __doc__ is not None
+    args: dict[str, str] = docopt(__doc__)
 
-    raw_path = Path(argv[1])
-    name = f"{raw_path.parent.stem}_{raw_path.stem}"
-    ext = raw_path.suffix.replace(".", "_")
+    print(args)
+    path = Path(args["<path>"])
+    name = f"{path.parent.stem}_{path.stem}"
+    ext = path.suffix.replace(".", "_")
 
-    path = Path(__file__).parent / "src" / str(raw_path)
+    fullpath = Path(__file__).parent / args["--parent"] / args["<path>"]
 
-    path.parent.mkdir(parents=True, exist_ok=True)
-    path.write_text(template.format(name=name.upper(), ext=ext.upper()))
+    fullpath.parent.mkdir(parents=True, exist_ok=True)
+    fullpath.write_text(
+        template.format(
+            name=name.upper(), ext=ext.upper(), namespace=args["--namespace"]
+        )
+    )
 
 
 if __name__ == "__main__":
