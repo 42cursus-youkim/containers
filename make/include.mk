d6df9e10ffffbe867a452a4fe5c727772eb3625a
scarf <greenscarf005@gmail.com>
2022-05-14 13:50:09 +0900

    fix: :building_construction: make includes nested for better structure

--- make/include.mk
+++ make/include.mk
@@ -1,8 +1,1 @@
-INC_CONTAINERS := containers/
-# $(addprefix containers/, \
-	map/ set/ stack/ vector/)
-
-INC_UTILS := $(addprefix utils/, \
-	general/ iter/ pair/ tmp/)
-
-INC = $(addprefix -I src/, $(INC_CONTAINERS) $(INC_UTILS))
+INC = $(addprefix -I src/,  containers/ utils/)
