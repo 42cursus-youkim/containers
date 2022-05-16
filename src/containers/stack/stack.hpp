717aecada68acacfc1f83cdec9849d4a5851072f
scarf <greenscarf005@gmail.com>
2022-05-16 09:21:50 +0900

    feat: :sparkles: stack done

--- src/containers/stack/stack.hpp
+++ src/containers/stack/stack.hpp
@@ -1,8 +1,93 @@
 #ifndef STACK_STACK_HPP
 #define STACK_STACK_HPP
 
+#include <deque>
+
 namespace ft {
+
+// TODO: change to ft::vector
+template <class T, class Container = std::deque<T> >
+class stack {
+ public:
+  typedef T value_type;
+  typedef Container container_type;
+  typedef size_t size_type;
+
+ protected:
+  container_type container_;
+
+ public:
+  explicit stack(const container_type& ctnr = container_type())
+      : container_(ctnr) {}
+
+  ~stack() {}
+
+  bool empty() const { return container_.empty(); }
+  size_type size() const { return container_.size(); }
+  value_type& top() { return container_.back(); }
+  const value_type& top() const { return container_.back(); }
+  void push(const value_type& val) { container_.push_back(val); }
+  void pop() { container_.pop_back(); }
+
+  template <class Tn, class ContainerN>
+  friend bool operator==(const stack<Tn, ContainerN>& lhs,
+                         const stack<Tn, ContainerN>& rhs);
+
+  template <class Tn, class ContainerN>
+  friend bool operator!=(const stack<Tn, ContainerN>& lhs,
+                         const stack<Tn, ContainerN>& rhs);
+
+  template <class Tn, class ContainerN>
+  friend bool operator<(const stack<Tn, ContainerN>& lhs,
+                        const stack<Tn, ContainerN>& rhs);
+
+  template <class Tn, class ContainerN>
+  friend bool operator<=(const stack<Tn, ContainerN>& lhs,
+                         const stack<Tn, ContainerN>& rhs);
+
+  template <class Tn, class ContainerN>
+  friend bool operator>(const stack<Tn, ContainerN>& lhs,
+                        const stack<Tn, ContainerN>& rhs);
+
+  template <class Tn, class ContainerN>
+  friend bool operator>=(const stack<Tn, ContainerN>& lhs,
+                         const stack<Tn, ContainerN>& rhs);
+};
+
+template <class T, class Container>
+bool operator==(const stack<T, Container>& lhs,
+                const stack<T, Container>& rhs) {
+  return lhs.container_ == rhs.container_;
+}
+
+template <class T, class Container>
+bool operator!=(const stack<T, Container>& lhs,
+                const stack<T, Container>& rhs) {
+  return lhs.container_ != rhs.container_;
+}
+
+template <class T, class Container>
+bool operator<(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+  return lhs.container_ < rhs.container_;
+}
+
+template <class T, class Container>
+bool operator<=(const stack<T, Container>& lhs,
+                const stack<T, Container>& rhs) {
+  return lhs.container_ <= rhs.container_;
+}
+
+template <class T, class Container>
+bool operator>(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+  return lhs.container_ > rhs.container_;
+}
+
+template <class T, class Container>
+bool operator>=(const stack<T, Container>& lhs,
+                const stack<T, Container>& rhs) {
+  return lhs.container_ >= rhs.container_;
+}
 
 }  // namespace ft
 
-#endif // STACK_STACK_HPP
+#endif  // STACK_STACK_HPP
---
+++ src/containers/stack/stack.tpp
@@ -0,0 +1,8 @@
+#ifndef STACK_STACK_TPP
+#define STACK_STACK_TPP
+
+namespace ft {
+
+}  // namespace ft
+
+#endif // STACK_STACK_TPP
