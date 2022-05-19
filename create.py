c92d9f32a65e55624101040d8cd352da61169973
scarf <greenscarf005@gmail.com>
2022-05-19 11:26:34 +0900

    feat: :sparkles: auto-add hpp include to tpp generation

--- create.py
+++ create.py
@@ -20,12 +20,24 @@ template = """\
 #ifndef {name}{ext}
 #define {name}{ext}
 
+{include}
+
 namespace {namespace} {{
 
 }}  // namespace {namespace}
 
 #endif // {name}{ext}
 """
+
+
+def base_category_name(stem: str) -> str:
+    return stem.rsplit("_", maxsplit=1)[0]
+
+
+def get_include(path: Path) -> str:
+    if path.suffix == ".tpp":
+        return f'#include "{base_category_name(path.stem)}.hpp"'
+    return ""
 
 
 def main():
@@ -36,15 +48,18 @@ def main():
     path = Path(args["<path>"])
     name = f"{path.parent.stem}_{path.stem}"
     ext = path.suffix.replace(".", "_")
+    include = get_include(path)
+
+    text = template.format(
+        name=name.upper(),
+        ext=ext.upper(),
+        namespace=args["--namespace"],
+        include=include,
+    )
 
     fullpath = Path(__file__).parent / args["--parent"] / args["<path>"]
-
     fullpath.parent.mkdir(parents=True, exist_ok=True)
-    fullpath.write_text(
-        template.format(
-            name=name.upper(), ext=ext.upper(), namespace=args["--namespace"]
-        )
-    )
+    fullpath.write_text(text)
 
 
 if __name__ == "__main__":
